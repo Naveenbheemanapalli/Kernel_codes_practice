eba91bfadc1accbe3dcf6185aa663958096b9974
Naveenbheemanapalli <naveen.bheemanapalli@gmail.com>
2020-08-04 08:00:09 +0530

    added panic kernel module in Linux

---
+++ panic/README.md
@@ -0,0 +1,23 @@
+-> To run the kernel module use the commands as follows
+1) make 
+2) sudo insmod ./panic_example.ko -> to initialize the module
+		Then this kernel module will be initialized
+		
+To use :-
+========
+
+
+Writing :-
+TO CREATE A PANIC IN THE KERNEL
+	echo "panic" > /sys/sasken/training
+	
+	
+	
+Reading if needed:-
+	cat /sys/sasken/training
+	
+then we get the output what we written in that file previously by default it is NULL
+
+3) sudo rmmod panic_example -> to remove the module
+
+Thanks for going through the code...!
---
+++ panic/panic_example.c
@@ -0,0 +1,86 @@
+/*Brief Description:
+ *
+ * This Simple kernel module to demo kernel panic in the Linux Kernel
+ *
+ * TO KNOW HOW TO RUN THIS MODULE PLEASE GO-THROUGH :- README FILE
+ */
+
+#include<linux/init.h>
+#include<linux/module.h>
+#include<linux/uaccess.h>			
+#include<linux/kobject.h>
+#include<linux/kernel.h>
+#include<linux/sysfs.h>
+#include<linux/device.h>
+#include<linux/cdev.h>
+#include<linux/fs.h>
+#include<linux/kdev_t.h>
+#include<linux/slab.h>			
+#include<linux/string.h>
+
+#define MODULE_NAME "sysfs_example"			//defining the name of the kernel module
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Bheemanapalli naveen Kumar");
+MODULE_DESCRIPTION("A simple device driver for Describing working of the Sysfs filesystem");
+MODULE_VERSION("1.0");
+
+static struct kobject	*sysfs_kobj=NULL;
+static volatile char training[20];
+
+/*show function of the training file which will be called when read from the file*/
+static ssize_t training_show(struct kobject *kobj,struct kobj_attribute *attr,char *buf){
+	return sprintf(buf,"%s\n\n",training);
+}
+
+/*store function of the training file which will be called when writting is done to the file*/
+static ssize_t training_store(struct kobject *kobj,struct kobj_attribute *attr,const char *buf,size_t count){
+	sscanf(buf,"%s",training);
+	if(strncmp(buf,"panic",5)==0){
+		pr_info("panic arriving...\n");
+		panic("paniced...\n"); //panicing/creating kernel oops with the built in panic function method
+		//*(int *)0 = 0; 						//creating a buggy kernel panic with the NULL pointer assignment method
+	}
+	return count;
+}
+
+/*setting the file attributes and permissions of the training file*/
+static struct kobj_attribute attribute = __ATTR(training,0660,training_show,training_store);
+
+/*sysfs_device_init() will be called when we initilizes(insmod) the kernel module */
+static int __init sysfs_device_init(void){
+	int error=0;
+	pr_info("%s : module registered successfully...\n",MODULE_NAME);
+	
+	sysfs_kobj = kobject_create_and_add("sasken",NULL);		//creating a directory in the name of sasken in the /sys/ folder and obtaining the kernel object for it
+	
+	if(!sysfs_kobj){																		//checking if kernel object is created or not
+		pr_info("unable create directory sasken folder in /sys/...\n");
+		return -ENOMEM;
+	}
+	else{
+		pr_info("sysfs_kobject created successfully ...\n");
+	}
+	
+	error = sysfs_create_file(sysfs_kobj,&attribute.attr);	//creating the training file with attributes and permissions defined in the struct attribute
+	if(error){
+		pr_info("Unable to create file in the /sys/sasken/ folder...\n");
+		return error;
+	}
+	return 0;
+}
+
+
+/*exiting and cleanup function which will be called when we remove the kernel module*/
+static void __exit sysfs_device_exit(void){
+	sysfs_remove_file(sysfs_kobj,&attribute.attr);	//removing the training file from the /sys/sasken folder
+	kobject_put(sysfs_kobj);		//decrementing the kernel object and releasing the resource own by that kernel object
+	kobject_del(sysfs_kobj);
+	pr_info("%s : module unregistered successfully..\n",MODULE_NAME);
+	
+}
+
+
+module_init(sysfs_device_init);			//intilization and exiting function registeration
+module_exit(sysfs_device_exit);
+
